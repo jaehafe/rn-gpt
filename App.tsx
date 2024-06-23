8e585f98ff9caa45459dfc7765e6b34e12a1f465
Jaeha Lee <wogkdkrm112@gmail.com>
2024-06-23 16:45:15 +0900

    feat: AnimatedIntro component

--- App.tsx
+++ App.tsx
@@ -5,20 +5,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Button from '@/components/Button';
+import AnimatedIntro from '@/components/AnimatedIntro';
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <GestureHandlerRootView style={{flex: 1}}> */}
-      <Text>
-        <Button>asdasdasdasdasd</Button>
-        <Button>asdasdasdasdasd</Button>
-        <Button>asdasdasdasdasd</Button>
-        <Button>asdasdasdasdasd</Button>
-      </Text>
-      {/* </GestureHandlerRootView> */}
+      <AnimatedIntro />
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({});
+
+{
+  /* <GestureHandlerRootView style={{flex: 1}}></GestureHandlerRootView> */
+}
