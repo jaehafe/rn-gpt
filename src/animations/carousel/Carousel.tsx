1ea8158fae7c5a1ed87d2968256c40276c97f71e
jaehafe <wogkdkrm112@gmail.com>
2024-07-04 17:03:18 +0900

    feat: carousel asset 추가

--- src/animations/carousel/Carousel.tsx
+++ src/animations/carousel/Carousel.tsx
@@ -23,55 +23,56 @@ import {runOnJS} from 'react-native-reanimated';
 
 const {width} = Dimensions.get('screen');
 
-const DATA = [
+type DataType = {
+  title: string;
+  location: string;
+  date: string;
+  poster: any;
+};
+
+const DATA: DataType[] = [
   {
     title: 'Afro vibes',
     location: 'Mumbai, India',
     date: 'Nov 17th, 2020',
-    poster:
-      'https://www.creative-flyers.com/wp-content/uploads/2020/07/Afro-vibes-flyer-template.jpg',
+    poster: require('./assets/1.jpg'),
   },
   {
     title: 'Jungle Party',
     location: 'Unknown',
     date: 'Sept 3rd, 2020',
-    poster:
-      'https://www.creative-flyers.com/wp-content/uploads/2019/11/Jungle-Party-Flyer-Template-1.jpg',
+    poster: require('./assets/1.jpg'),
   },
   {
     title: '4th Of July',
     location: 'New York, USA',
     date: 'Oct 11th, 2020',
-    poster:
-      'https://www.creative-flyers.com/wp-content/uploads/2020/06/4th-Of-July-Invitation.jpg',
+    poster: require('./assets/1.jpg'),
   },
   {
     title: 'Summer festival',
     location: 'Bucharest, Romania',
     date: 'Aug 17th, 2020',
-    poster:
-      'https://www.creative-flyers.com/wp-content/uploads/2020/07/Summer-Music-Festival-Poster.jpg',
+    poster: require('./assets/1.jpg'),
   },
+
   {
     title: 'BBQ with friends',
     location: 'Prague, Czech Republic',
     date: 'Sept 11th, 2020',
-    poster:
-      'https://www.creative-flyers.com/wp-content/uploads/2020/06/BBQ-Flyer-Psd-Template.jpg',
+    poster: require('./assets/1.jpg'),
   },
   {
     title: 'Festival music',
     location: 'Berlin, Germany',
     date: 'Apr 21th, 2021',
-    poster:
-      'https://www.creative-flyers.com/wp-content/uploads/2020/06/Festival-Music-PSD-Template.jpg',
+    poster: require('./assets/1.jpg'),
   },
   {
     title: 'Beach House',
     location: 'Liboa, Portugal',
     date: 'Aug 12th, 2020',
-    poster:
-      'https://www.creative-flyers.com/wp-content/uploads/2020/06/Summer-Beach-House-Flyer.jpg',
+    poster: require('./assets/1.jpg'),
   },
 ];
 
@@ -114,7 +115,7 @@ const OverflowItems = ({data, scrollXAnimated}: OverflowItemsProps) => {
                     color="black"
                     style={{marginRight: 5}}
                   /> */}
-                  123
+
                   {item.location}
                 </Text>
                 <Text style={[styles.date]}>{item.date}</Text>
@@ -136,7 +137,6 @@ export default function Carousel() {
     scrollXIndex.setValue(activeIndex);
     setIndex(activeIndex);
   }, []);
-  const fling = Gesture.Fling();
 
   React.useEffect(() => {
     if (index === data.length - VISIBLE_ITEMS - 1) {
@@ -200,11 +200,9 @@ export default function Carousel() {
                 style,
                 ...props
               }) => {
-                console.log('style>>', style);
-
                 const newStyle = [style, {zIndex: data.length - index}];
                 return (
-                  <View style={newStyle} index={index} {...props}>
+                  <View style={newStyle} index={index} {...props} key={index}>
                     {children}
                   </View>
                 );
@@ -226,6 +224,7 @@ export default function Carousel() {
 
                 return (
                   <Animated.View
+                    key={index}
                     style={{
                       position: 'absolute',
                       left: -ITEM_WIDTH / 2,
@@ -239,7 +238,7 @@ export default function Carousel() {
                     }}
                   >
                     <Image
-                      source={{uri: item.poster}}
+                      source={item.poster}
                       style={{
                         width: ITEM_WIDTH,
                         height: ITEM_HEIGHT,
