f8c0e067a07a0d35bc643e0d9ba877eb6887ff19
Jaeha Lee <wogkdkrm112@gmail.com>
2024-07-19 00:03:00 +0900

    feat: ios widget 구현

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import {StyleSheet} from 'react-native';
-import React, {useEffect} from 'react';
+import {NativeModules, StyleSheet, ToastAndroid} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import 'react-native-gesture-handler';
@@ -19,7 +19,30 @@ import {QueryClientProvider} from '@tanstack/react-query';
 import queryClient from '@/apis/queryClient';
 import SplashScreen from 'react-native-splash-screen';
 
+import SharedGroupPreferences from 'react-native-shared-group-preferences';
+import Button from '@/components/ui/Button';
+const group = 'group.rngpt';
+const SharedStorage = NativeModules.SharedStorage;
+
 export default function App() {
+  const [text, setText] = useState('');
+  const widgetData = {
+    text,
+  };
+
+  const handleSubmit = async () => {
+    try {
+      // iOS
+      await SharedGroupPreferences.setItem('widgetKey', widgetData, group);
+    } catch (error) {
+      console.log({error});
+    }
+    const value = `${text} days`;
+    // Android
+    SharedStorage.set(JSON.stringify({text: value}));
+    ToastAndroid.show('Change value successfully!', ToastAndroid.SHORT);
+  };
+
   useEffect(() => {
     requestUserPermission();
     notificationListener();
@@ -48,6 +71,8 @@ export default function App() {
         <QueryClientProvider client={queryClient}>
           <SafeAreaProvider>
             <RootNavigator />
+
+            {/* <Button onPress={handleSubmit}>Submit</Button> */}
           </SafeAreaProvider>
         </QueryClientProvider>
       </GestureHandlerRootView>
---
+++ ios/RngptWidget/RngptWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RngptWidgetBundle.swift
+//  RngptWidget
+//
+//  Created by LEE JAEHA on 7/18/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RngptWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RngptWidget()
+    }
+}
