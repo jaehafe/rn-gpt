2139765fdb14293f405658dbd62e01141e30b7dc
Jaeha Lee <wogkdkrm112@gmail.com>
2024-07-20 11:31:02 +0900

    feat: widget 생성 input, button 임시 구현

--- src/components/chat/Explore.tsx
+++ src/components/chat/Explore.tsx
@@ -1,17 +1,29 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 
 import {RngptSharedDefaults} from '@/native-modules/widget';
+import Button from '../ui/Button';
+import {defaultStyles} from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 export default function Explore() {
   const rngptModules = new RngptSharedDefaults();
 
-  const todo = 'data 동적 추가 reloadAllTimelines';
+  const [widgetText, setWidgetText] = useState('initial widget data');
+  const [inputText, setInputText] = useState('');
 
   useEffect(() => {
     const updateWidget = async () => {
       try {
-        const widgetData = {rngptWidgetData: JSON.stringify({todo: todo})};
+        const widgetData = {
+          rngptWidgetData: JSON.stringify({todo: widgetText}),
+        };
         await rngptModules.set(widgetData);
 
         console.log('Widget data updated:', widgetData);
@@ -20,16 +32,51 @@ export default function Explore() {
       }
     };
 
-    if (todo) {
+    if (widgetText) {
       updateWidget();
     }
-  }, [todo]);
+  }, [widgetText]);
+
+  const handleSave = () => {
+    setWidgetText(inputText);
+  };
 
   return (
     <View>
-      <Text>{'data'}</Text>
+      <TextInput
+        style={styles.input}
+        value={inputText}
+        onChangeText={setInputText}
+        placeholder="Enter widget data"
+      />
+      {/* <Button onPress={handleSave}>
+        <Text>Widget Text Save</Text>
+      </Button> */}
+      <TouchableOpacity
+        style={[defaultStyles.btn, styles.btnPrimary]}
+        onPress={handleSave}
+      >
+        <Text style={styles.btnPrimaryText}>Create account</Text>
+      </TouchableOpacity>
+      <Text>{widgetText}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+  btnPrimary: {
+    backgroundColor: Colors.primary,
+    marginVertical: 4,
+  },
+  btnPrimaryText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
