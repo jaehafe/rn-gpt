53d3b8af0540cc943d62833c705dd721bd8a4419
Jaeha Lee <wogkdkrm112@gmail.com>
2024-07-26 00:39:16 +0900

    chore: minor update

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
     Platform.OS === 'android'
       ? 'http://10.0.2.2:8080'
       : 'http://localhost:8080',
-  // baseURL: 'http://localhost:8080',
+
   withCredentials: true,
 });
 
@@ -32,7 +32,7 @@ axiosInstance.interceptors.response.use(
           {withCredentials: true},
         );
         const {accessToken} = response.data;
-        console.log('New accessToken:', accessToken); // 여기에 로그 추가
+        console.log('New accessToken:', accessToken);
         await AsyncStorage.setItem('accessToken', accessToken);
         axiosInstance.defaults.headers.common[
           'Authorization'
@@ -52,9 +52,6 @@ async function handleLogout() {
   try {
     await AsyncStorage.removeItem('accessToken');
     await AsyncStorage.setItem('isLoggedIn', 'false');
-    // 여기에 전역 상태 업데이트 로직 추가
-    // 예: dispatch(logoutAction());
-    // 또는 이벤트 발생: EventEmitter.emit('logout');
   } catch (error) {
     console.error('Logout error:', error);
   }
